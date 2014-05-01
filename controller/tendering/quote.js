d6f0d64c6b639a8f6ddb08847f5c84bd60c40af9
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-05-01 13:01:18 +0530

    api tendering_teams_quote modified status_code

--- controller/tendering/quote.js
+++ controller/tendering/quote.js
@@ -1,5 +1,5 @@
 exports.tendering_teams_quotes = function(req, res){
-	var query="SELECT `rfq`.`id`, `rfq`.`document_no`, `rfq`.`version_no`, `rfq`.`date_rfq_in`, `rfq`.requested_quotation_date FROM `rfq` WHERE `rfq_status_id`='3' ORDER BY `rfq`.`updated_at` desc";
+	var query="SELECT `rfq`.`id`, `rfq`.`document_no`, `rfq`.`version_no`, `rfq`.`date_rfq_in`, `rfq`.requested_quotation_date FROM `rfq` WHERE `rfq_status_id`='4' ORDER BY `rfq`.`updated_at` desc";
 	connection.query(query, function(err, rfq) {
 		if(err){
 			console.log(err);
@@ -10,3 +10,4 @@ exports.tendering_teams_quotes = function(req, res){
 		}
 	});
 }
+
