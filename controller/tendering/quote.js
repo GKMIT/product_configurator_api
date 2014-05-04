c302b5df80ea39600f117aef21acad3d6dea5915
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-05-05 01:17:45 +0530

    api with modified json format

--- controller/tendering/quote.js
+++ controller/tendering/quote.js
@@ -29,39 +29,69 @@ exports.tendering_fetch_particular_quote = function(req, res){
 						res.json({"statusCode": 500, "success":"false", "message": "22internal error"});
 				}
 				else{
-					connection.query("SELECT * FROM `rfq_lines_technical_specs` `rlts` LEFT JOIN `product_properties` `pp` ON `rlts`.`product_properties_id`=`pp`.id WHERE `rfq_lines_id`='"+rfq_lines[0].id+"'", function(err, rfq_lines_technical_specs) {
-						if(err){
-							console.log(err);
-								res.json({"statusCode": 500, "success":"false", "message": "33internal error"});
-						}
-						else{
-							var temp=rfq_lines[0].month/3;
-							// console.log(temp);
-							var quarter;
-							if(temp<=1){
-								quarter=1;
-							}
-							else if(temp>1 && temp<=2){
-								quarter=2;
-							}
-							else if(temp>2 && temp<=3){
-								quarter=3;
+					var merge = function() {
+				    	var obj = {},
+				        i = 0,
+				        il = arguments.length,
+				        key;
+					    for (; i < il; i++) {
+					        for (key in arguments[i]) {
+					            if (arguments[i].hasOwnProperty(key)) {
+					                obj[key] = arguments[i][key];
+					            }
+					        }
+					    }
+					    return obj;
+					};
+					var complete_rfq_lines=rfq_lines;
+					var counter=0;
+					var counter1=0;
+					for (var i = 0; i < rfq_lines.length; i++) {
+						connection.query("SELECT * FROM `rfq_lines_technical_specs` `rlts` LEFT JOIN `product_properties` `pp` ON `rlts`.`product_properties_id`=`pp`.id WHERE `rfq_lines_id`='"+rfq_lines[counter].id+"'", function(err, rfq_lines_technical_specs) {
+							if(err){
+								console.log(err);
+									res.json({"statusCode": 500, "success":"false", "message": "internal error"});
 							}
 							else{
-								quarter=4;
-							}
-							console.log("SELECT * FROM `product_designs` LEFT JOIN `product_designs_costs` ON `product_designs`.id=`product_designs_costs`.`product_design_id` AND `product_designs_costs`.`quarter`='"+quarter+"' AND `product_designs_costs`.`year`='"+rfq_lines[0].year+"' WHERE `product_designs`.id='"+rfq_lines[0].product_designs_id+"'");
-							connection.query("SELECT * FROM `product_designs` LEFT JOIN `product_designs_costs` ON `product_designs`.id=`product_designs_costs`.`product_design_id` AND `product_designs_costs`.`quarter`='"+quarter+"' AND `product_designs_costs`.`year`='"+rfq_lines[0].year+"' WHERE `product_designs`.id='"+rfq_lines[0].product_designs_id+"'", function(err, product_design_detail) {
-								if(err){
-									console.log(err);
-										res.json({"statusCode": 500, "success":"false", "message": "44internal error"});
+								// console.log(i);
+								var temp=rfq_lines[counter].month/3;
+								var quarter;
+								if(temp<=1){
+									quarter=1;
+								}
+								else if(temp>1 && temp<=2){
+									quarter=2;
+								}
+								else if(temp>2 && temp<=3){
+									quarter=3;
 								}
 								else{
-									res.json({"statusCode": 200, "success":"true", "message": "", "rfq":rfq, "rfq_lines":rfq_lines, "rfq_lines_technical_specs": rfq_lines_technical_specs, "product_design_detail":product_design_detail});
+									quarter=4;
 								}
-							});
-						}
-					});
+								console.log(counter1);
+								// console.log(rfq_lines[counter].product_designs_id);
+								console.log("counter is : "+counter);
+								connection.query("SELECT * FROM `product_designs` LEFT JOIN `product_designs_costs` ON `product_designs`.id=`product_designs_costs`.`product_design_id` AND `product_designs_costs`.`quarter`='"+quarter+"' AND `product_designs_costs`.`year`='"+rfq_lines[counter1].year+"' WHERE `product_designs`.id='"+rfq_lines[counter1].product_designs_id+"'", function(err, product_design_detail) {
+									if(err){
+										console.log(err);
+											res.json({"statusCode": 500, "success":"false", "message": "44internal error"});
+									}
+									else{
+										console.log("counter is ===>  "+counter);
+										complete_rfq_lines[counter]["rfq_lines_technical_specs"]=rfq_lines_technical_specs;
+										complete_rfq_lines[counter]["product_design_detail"]=product_design_detail;
+										counter++;
+										if(counter==rfq_lines.length){
+											res.json({"statusCode": 200, "success":"true", "message": "","rfq":rfq ,"rfq_lines":complete_rfq_lines});
+										}
+									}
+								});
+								counter1++;
+							}
+						});
+					};
+					
+					
 				}
 			});
 		}
