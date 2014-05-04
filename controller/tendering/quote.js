209615a5295de52f827c6ea1996b4cbe90f54ffd
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-05-04 17:19:15 +0530

    api for the tendering_fetch_particular_quote

--- controller/tendering/quote.js
+++ controller/tendering/quote.js
@@ -13,44 +13,48 @@ exports.tendering_teams_quotes = function(req, res){
 
 
 exports.tendering_fetch_particular_quote = function(req, res){
-	var query="SELECT `rfq`.`id`, `rfq`.`document_no`, `rfq`.`version_no`, `rfq`.`rfq_status_id` FROM `rfq` WHERE `rfq_status_id`='4' where `id`='"+req.body.rfq_id+"'";
+	var query="SELECT `rfq`.`id`, `rfq`.`document_no`, `rfq`.`version_no`, `rfq`.`rfq_status_id` FROM `rfq` WHERE `rfq_status_id`='4' AND `id`='"+req.params.rfq_id+"'";
+	// console.log(query);
 	connection.query(query, function(err, rfq) {
 		if(err){
 			console.log(err);
-				res.json({"statusCode": 500, "success":"false", "message": "internal error"});
+				res.json({"statusCode": 500, "success":"false", "message": "11internal error"});
 		}
 		else{
 			 // `r_lines`.`material_cost`, `r_lines`.`labour_cost`, `r_lines`.`no_of_labour_hours`, 
-			connection.query("SELECT `r_lines`.`id`, `r_lines`.`product_lines_id`, `p_lines`.`name` as `product_lines_name`, `r_lines`.`plants_id`, `plants`.`name`, as `plants_name` `r_lines`.`number_of_units`, EXTRACT(YEAR FROM `r_lines`.`req_delivery_date`) as year, `r_lines`.`req_delivery_date`, `r_lines`.`sales_price`, `r_lines`.`confirmed_delivery_date`, `r_lines`.`product_designs_id`  FROM `rfq_lines` `r_lines`, LEFT JOIN `product_lines` `p_lines` ON `r_lines`.`product_lines_id`=`p_lines`.`id` LEFT JOIN `plants` ON `r_lines`.`plants_id`=`plants`.`id` LEFT JOIN `product_designs` ON `r_lines`.`product_designs_id`=`product_designs`.`id`  WHERE `rfq_id`='"+req.body.rfq_id+"'", function(err, rfq_lines) {
+
+			connection.query("SELECT `r_lines`.`id`, `r_lines`.`product_lines_id`, `p_lines`.`name` as `product_lines_name`, `r_lines`.`plants_id`, `plants`.`name` as `plants_name`, `r_lines`.`number_of_units`,`r_lines`.`req_delivery_date`, EXTRACT(MONTH FROM req_delivery_date) as month, EXTRACT(YEAR FROM req_delivery_date) as year, `r_lines`.`sales_price`, `r_lines`.`confirmed_delivery_date`, `r_lines`.`product_designs_id` FROM `rfq_lines` `r_lines` LEFT JOIN `product_lines` `p_lines` ON `r_lines`.`product_lines_id`=`p_lines`.`id` LEFT JOIN `plants` ON `r_lines`.`plants_id`=`plants`.`id` LEFT JOIN `product_designs` ON `r_lines`.`product_designs_id`=`product_designs`.`id`  WHERE `rfq_id`='"+req.params.rfq_id+"'", function(err, rfq_lines) {
 				if(err){
 					console.log(err);
-						res.json({"statusCode": 500, "success":"false", "message": "internal error"});
+						res.json({"statusCode": 500, "success":"false", "message": "22internal error"});
 				}
 				else{
-					connection.query("SELECT FROM `rfq_lines_technical_specs` `rlts` LEFT JOIN `product_properties` `pp` ON `rlts`.`product_properties_id`=`pp`.id WHERE `rfq_lines_id`='"+rfq_lines[0].id+"'", function(err, rfq_lines_technical_specs) {
+					connection.query("SELECT * FROM `rfq_lines_technical_specs` `rlts` LEFT JOIN `product_properties` `pp` ON `rlts`.`product_properties_id`=`pp`.id WHERE `rfq_lines_id`='"+rfq_lines[0].id+"'", function(err, rfq_lines_technical_specs) {
 						if(err){
 							console.log(err);
-								res.json({"statusCode": 500, "success":"false", "message": "internal error"});
+								res.json({"statusCode": 500, "success":"false", "message": "33internal error"});
 						}
 						else{
-							var temp=rfq_lines[0].year/3;
+							var temp=rfq_lines[0].month/3;
+							// console.log(temp);
 							var quarter;
-							if(quarter<=1){
+							if(temp<=1){
 								quarter=1;
 							}
-							else if(quarter>1 && quarter<=2){
+							else if(temp>1 && temp<=2){
 								quarter=2;
 							}
-							else if(quarter>2 && quarter<=3){
+							else if(temp>2 && temp<=3){
 								quarter=3;
 							}
 							else{
 								quarter=4;
 							}
-							connection.query("SELECT * FROM `product_designs` LEFT JOIN `product_designs_costs` ON `product_designs`.id=`product_designs_costs`.`product_designs_id` WHERE `product_designs_costs`.`quarter`='"+quarter+"'", function(err, product_design_detail) {
+							console.log("SELECT * FROM `product_designs` LEFT JOIN `product_designs_costs` ON `product_designs`.id=`product_designs_costs`.`product_design_id` AND `product_designs_costs`.`quarter`='"+quarter+"' AND `product_designs_costs`.`year`='"+rfq_lines[0].year+"' WHERE `product_designs`.id='"+rfq_lines[0].product_designs_id+"'");
+							connection.query("SELECT * FROM `product_designs` LEFT JOIN `product_designs_costs` ON `product_designs`.id=`product_designs_costs`.`product_design_id` AND `product_designs_costs`.`quarter`='"+quarter+"' AND `product_designs_costs`.`year`='"+rfq_lines[0].year+"' WHERE `product_designs`.id='"+rfq_lines[0].product_designs_id+"'", function(err, product_design_detail) {
 								if(err){
 									console.log(err);
-										res.json({"statusCode": 500, "success":"false", "message": "internal error"});
+										res.json({"statusCode": 500, "success":"false", "message": "44internal error"});
 								}
 								else{
 									res.json({"statusCode": 200, "success":"true", "message": "", "rfq":rfq, "rfq_lines":rfq_lines, "rfq_lines_technical_specs": rfq_lines_technical_specs, "product_design_detail":product_design_detail});
--- controller/validation/tendering/quote.js
+++ controller/validation/tendering/quote.js
@@ -29,4 +29,42 @@ exports.tendering_teams_quotes = function(req, res, next){
 			}
 		});
 	}
-}
+};
+
+
+
+exports.tendering_fetch_particular_quote = function(req, res, next){
+	var checkValid=1;
+	if(req.header("authentication_token")=="" || typeof req.header("authentication_token")=="undefined"){
+		checkValid=0;
+		res.json({"statusCode": 404, "success":"false", "message": "authentication_token not found"});
+	}
+	else if(typeof req.params.user_id=="undefined"){
+		checkValid=0;
+		res.json({"statusCode": 404, "success":"false", "message": "user_id not undefined"});
+	}
+	else if (req.params.user_id=="" || !validator.isNumeric(req.params.user_id)){
+		checkValid=0;
+		res.json({"statusCode": 404, "success":"false", "message": "user_id not found"});
+	}
+	else if (req.params.rfq_id=="" || !validator.isNumeric(req.params.rfq_id)){
+		checkValid=0;
+		res.json({"statusCode": 404, "success":"false", "message": "user_id not found"});
+	}
+	else if(checkValid==1){
+		connection.query("SELECT `id`, `authentication_token` FROM `organization_users` WHERE `authentication_token`='"+req.header("authentication_token")+"' AND `id`="+req.params.user_id, function(err, organization_users) {
+			if(err){
+				console.log(err);
+					res.json({"statusCode": 500, "success":"false", "message": "internal error"});
+			}
+			else{
+				if (organization_users.length>0) {
+						next();				
+				}
+				else{
+					res.json({"statusCode": 404, "success":"false", "message": "user not found"});
+				}
+			}
+		});
+	}
+};
