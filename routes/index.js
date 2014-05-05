1808b864aa0e2c7ae7d2f3d6f10bd3653c0c62b1
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-05-05 15:10:58 +0530

    modify the save and update calls for rfq_lines remarks not manditory in technical specification

--- routes/index.js
+++ routes/index.js
@@ -79,6 +79,8 @@ module.exports = function(){
 
 
 	// api for the sales_person for the quote finalize and follow up
-
+	var rfq_quote_calls=require('../controller/rfq/rfq_quote_finalize.js');
+	var rfq_quote_validation=require('../controller/validation/rfq_quote_finalize.js');
+	app.get("/sales_quote_finalize_fetch_all/:user_id", rfq_quote_validation.sales_quote_finalize_fetch_all, rfq_quote_calls.sales_quote_finalize_fetch_all);
 
 };
