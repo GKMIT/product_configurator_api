c0c31ded8ed5c39cee501654519542e02c79bd35
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-05-05 16:09:05 +0530

    api for the sales_quote_finalize_one rfq detail

--- controller/rfq/rfq_quote_finalize.js
+++ controller/rfq/rfq_quote_finalize.js
@@ -10,3 +10,16 @@ exports.sales_quote_finalize_fetch_all = function(req, res){
 		}
 	});
 }
+
+exports.sales_quote_finalize_fetch_one = function(req, res){
+	var query="SELECT `id`, `document_no`, `version_no`, `date_rfq_in`, `created_at`, `estimated_sales_price`, `probability` FROM `rfq` WHERE `created_by`='"+req.params.user_id+"' AND `rfq_status_id`='5'  ORDER BY `rfq`.`updated_at` desc";
+	connection.query(query, function(err, rfq) {
+		if(err){
+			console.log(err);
+				res.json({"statusCode": 500, "success":"false", "message": "internal error"});
+		}
+		else{
+			res.json({"statusCode": 200, "success":"true", "message": "", "rfq":rfq});
+		}
+	});
+}
