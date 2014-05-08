4515c03e08822a2d7d8789f8cfacefb709dc6d21
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-05-08 12:44:15 +0530

    specs for the save_rfq_questions api call

--- test/rfq/bid_no_bid_spec.js
+++ test/rfq/bid_no_bid_spec.js
@@ -67,7 +67,7 @@ describe('ready_rfq_bid API Calls', function () {
 	});
 });
 
-//:user_id/:rfq_id
+
 
 describe('ready_rfq_bid_detail API Calls', function () {
 	var url="/ready_rfq_bid_detail"
@@ -186,6 +186,445 @@ describe('ready_rfq_bid_detail API Calls', function () {
 
 });
 
+// save_rfq_questions
+
+describe('save_rfq_questions API Call', function () {
+	var url="/save_rfq_questions"
+	it("Should ok All Correct Data", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 200, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When token not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						PostcallWithoutToken(url, parameter, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When authentication_token value not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, "", 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When authentication_token value invalid", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, "invalid", 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When user_id provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When user_id value not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':"", 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 404 , function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When invalid user_id provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':"0000", 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+
+	it("Should NOT OK When user_id value NonNumeric provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':"Nan", 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When rfq_id not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When rfq_id value not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': "", 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When NonNumeric rfq_id provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': "Nan", 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When questions not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When questions value not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':''};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When question non Array field provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':"Hello"};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When question array not proper means like : in array question_id field provide but value field not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When question array not proper means like :in array question_id field's value not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': '', 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When question array not proper means like :in array value field nonNumeric", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': rfq_detail.rfq_questions[i].id, 'value': 'NaN'};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+	it("Should NOT OK When question array not proper means like :in array question_id nonNumeric", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall("/ready_rfq_bid", parameter, token, 200, function(rfq){
+				 parameter=user_id+"/"+rfq.rfq[0].id;
+				getcall("/ready_rfq_bid_detail", parameter, token, 200, function(rfq_detail){
+					var questions = new Array();
+					for (var i = 0; i < rfq_detail.rfq_questions.length; i++) {
+						questions[i]={'question_id': "NaN", 'value': 1};
+					
+					};
+						if(i==rfq_detail.rfq_questions.length){
+						parameter={'user_id':user_id, 'rfq_id': rfq.rfq[0].id, 'questions':questions};
+						Postcall(url, parameter, token, 404, function(obj){
+							done();
+						});
+					}
+				});
+			});
+		});
+	});
+
+});
+
 function login(email, password, callback){
 	supertest(app)
 		.post('/login')
@@ -220,4 +659,32 @@ function getcallWithoutToken(url, parameter, status, callback){
 				res.body.statusCode.should.equal(status);
 			callback(res.body);
 		});
+}
+
+function Postcall(url, parameter, token, status, callback){
+	supertest(app)
+		.post(url)
+		.send(parameter)
+		.set('authentication_token', token)
+		.expect(status)
+		.end(function (err, res) {
+			if(err){
+			}
+			res.body.statusCode.should.equal(status);
+			callback(res.body);
+	});	
+}
+
+function PostcallWithoutToken(url, parameter, status, callback){
+	supertest(app)
+		.post(url)
+		.send(parameter)
+		// .set('authentication_token', token)
+		.expect(status)
+		.end(function (err, res) {
+			if(err){
+			}
+			res.body.statusCode.should.equal(status);
+			callback(res.body);
+	});	
 }
