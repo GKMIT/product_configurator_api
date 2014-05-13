1e10bfad1f7be1cca11caee20ffa656753f8b154
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-05-13 16:27:27 +0530

    specs for the tendering_teams_quote api

---
+++ test/tendering/quote_spec.js
@@ -0,0 +1,107 @@
+var app = require('../../helper/app');
+var testConnection = require('../../helper/connection');
+
+var should = require('should'),
+	supertest = require('supertest');
+
+	var global=require('../global/global_spec');
+	var email="nitin.naik@cgglobal.com";
+	var password="5e8ff9bf55ba3508199d22e984129be6";
+
+describe('tendering_teams_quotes API Calls', function () {
+
+	var url="/tendering_teams_quotes"
+	it("Should ok All Correct Data", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall(url, parameter, token, 200, function(rfq){
+				// console.log(rfq);
+				done();
+			});
+		});
+	});
+
+	it("Should NOT OK When token not provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcallWithoutToken(url, parameter, 404, function(rfq){
+				done();
+			});
+		});
+	});
+
+	it("Should NOT OK When invalid token pass", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter=user_id;
+			getcall(url, parameter, "invalid_token", 404, function(rfq){
+				done();
+			});
+		});
+	});
+
+	it("Should NOT OK When user_id not invalid provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter="101";
+			getcall(url, parameter, token, 404, function(rfq){
+				done();
+			});
+		});
+	});
+
+	it("Should NOT OK When NonNumeric user_id provide", function (done) {
+		login(email, password, function(user){
+			var user_id=user.data[0].id;
+			var token=user.authentication_token;
+			var parameter="NonNumeric";
+			getcall(url, parameter, token, 404, function(rfq){
+				done();
+			});
+		});
+	});
+});
+
+
+
+function login(email, password, callback){
+	supertest(app)
+		.post('/login')
+		.type('form')
+		.field('email', email)
+     	.field('password', password)
+		.end(function (err, res) {
+			if(err){
+			}
+			callback(res.body);
+	});
+}
+
+function getcall(url, parameter, token, status, callback){
+	supertest(app)
+	.get(url+"/"+parameter)
+	.set('authentication_token', token)
+	.expect(status)
+	.end(function (err, res) {
+		// console.log(err);
+		// console.log(res.body);
+		res.body.statusCode.should.equal(status);
+		callback(res.body);
+	});
+}
+
+function getcallWithoutToken(url, parameter, status, callback){
+	supertest(app)
+		.get(url+"/"+parameter)
+		.expect(status)
+		.end(function (err, res) {
+				res.body.statusCode.should.equal(status);
+			callback(res.body);
+		});
+}
