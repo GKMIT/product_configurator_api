89aa6da2d22d8d68fb4517f06f0b552c953f31f6
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-05-30 16:58:49 +0530

    modification in sales_quote_finalize_fetch_one api for the probability

--- controller/rfq/rfq_quote_finalize.js
+++ controller/rfq/rfq_quote_finalize.js
@@ -12,14 +12,22 @@ exports.sales_quote_finalize_fetch_all = function(req, res){
 }
 
 exports.sales_quote_finalize_fetch_one = function(req, res){
-	var query="SELECT `id`, `document_no`, `version_no`, `quote_creation_date`, `estimated_sales_price`, `probability` FROM `rfq` WHERE `created_by`='"+req.params.user_id+"' AND `rfq_status_id`='5'  ORDER BY `rfq`.`updated_at` desc";
+	var query="SELECT `id`, `document_no`, `version_no`, `quote_creation_date`, `estimated_sales_price`, `probability_id` FROM `rfq` WHERE `created_by`='"+req.params.user_id+"' AND `rfq_status_id`='5'  ORDER BY `rfq`.`updated_at` desc";
 	connection.query(query, function(err, rfq) {
 		if(err){
 			console.log(err);
 				res.json({"statusCode": 500, "success":"false", "message": "internal error"});
 		}
 		else{
-			res.json({"statusCode": 200, "success":"true", "message": "", "rfq":rfq});
+			connection.query("SELECT `id`, `name`, `value` FROM `probability`", function(err, probability) {
+				if(err){
+					console.log(err);
+						res.json({"statusCode": 500, "success":"false", "message": "internal error"});
+				}
+				else{
+					res.json({"statusCode": 200, "success":"true", "message": "", "rfq":rfq, "probability":probability});
+				}
+			});
 		}
 	});
 }
