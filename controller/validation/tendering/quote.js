7fb9f346121953abe29c97a02ae0504333e23763
chandrakantpaliwal <paliwal.ck@gmail.com>
2014-06-10 17:56:38 +0530

    api for get the overhead from complexities_id

--- controller/validation/tendering/quote.js
+++ controller/validation/tendering/quote.js
@@ -280,7 +280,45 @@ exports.tendering_calculate_sales_price = function(req, res, next){
 	}
 };
 
-
+exports.tendering_complexity_overhead = function(req, res, next){
+	var checkValid=1;
+	var fields = ["user_id", "complexities_id", "plants_id"];
+	// var fields = ["user_id", "rfq_lines_id", "complexities_id", "product_design_id"];
+	if(typeof req.header("authentication_token")=="undefined" || req.header("authentication_token")==""){
+		checkValid=0;
+		res.json({"statusCode": 404, "success": "false", "message": "Authentication token not found"});
+	}
+	else if(checkValid==1){
+		for(var i=0; i<fields.length; i++){
+			if(typeof req.params[fields[i]]=="undefined"){
+				checkValid=0;
+				res.json({"statusCode": 404, "success": "false", "message": fields[i]+" not defined"});
+				break;
+			}
+			if(req.params[fields[i]]==""  || !validator.isNumeric(req.params[fields[i]]) || req.params[fields[i]]==0){
+				checkValid=0;
+				res.json({"statusCode": 404, "success": "false", "message": fields[i]+" value not found"});
+				break;
+			}
+		}
+	}
+	if(checkValid==1){
+		connection.query("SELECT `id`, `authentication_token` FROM `organization_users` WHERE `authentication_token`='"+req.header("authentication_token")+"' AND `id`="+req.params.user_id, function(err, organization_users) {
+			if(err){
+				console.log(err);
+					res.json({"statusCode": 500, "success":"false", "message": "internal error"});
+			}
+			else{
+				if (organization_users.length>0) {
+						next();				
+				}
+				else{
+					res.json({"statusCode": 404, "success":"false", "message": "user not found"});
+				}
+			}
+		});
+	}
+};
 
 exports.tendering_save_calculated_sales_price = function(req, res, next){
 	var checkValid=1;
