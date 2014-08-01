90f2c874f5c654db4dbff0c2c6f647b34f5c6e68
Chnadra Kant Paliwal <chnadrakantpaliwal@Chnadras-MacBook-Air.local>
2014-08-01 18:26:58 +0530

    api modification for the bid_no_bid for sales_person name and customer_name

--- controller/rfq/bid_no_bid.js
+++ controller/rfq/bid_no_bid.js
@@ -7,7 +7,7 @@ exports.ready_rfq_bid = function(req, res){
 		}
 		else{
 			if(admin.length>0){
-				query="SELECT `rfq`.`id`, `rfq`.`document_no`, `rfq`.`version_no`, `rfq`.`date_rfq_in`, `rfq`.`sales_agents_id`, `agent`.`name` FROM `rfq` LEFT JOIN `sales_agents` `agent` ON `rfq`.`sales_agents_id`=`agent`.`id` WHERE `rfq_status_id`='2' ORDER BY `rfq`.`updated_at` desc";
+				query="SELECT `rfq`.`id`, `rfq`.`document_no`, `rfq`.`version_no`, `rfq`.`date_rfq_in`, `rfq`.`sales_agents_id`, `agent`.`name` as ` sales_agent_name`, `customers`.`name` as `customer_name`, `organization_users`.`full_name` as `sales_person_name` FROM `rfq` LEFT JOIN `sales_agents` `agent` ON `rfq`.`sales_agents_id`=`agent`.`id` LEFT JOIN `customers` ON `customers`.`id`=`rfq`.`customers_id` LEFT JOIN `organization_users` on `organization_users`.`id`=`rfq`.`sales_person_id` WHERE `rfq`.`rfq_status_id`='2' ORDER BY `rfq`.`updated_at` desc";
 				connection.query(query, function(err, rfq) {
 					if(err){
 						console.log(err);
